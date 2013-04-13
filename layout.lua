454540f1c8a630d5d4646ced11e01676ef42c3e2
David Sveningsson <ext@sidvind.com>
2013-04-13 16:54:36 +0200

    different default layout per screen

--- layout.lua
+++ layout.lua
@@ -7,16 +7,27 @@ layouts = {
 	awful.layout.suit.tile.top,
 	awful.layout.suit.fair,
 	awful.layout.suit.fair.horizontal,
-	awful.layout.suit.spiral,
-	awful.layout.suit.spiral.dwindle,
-	awful.layout.suit.max,
+--	awful.layout.suit.spiral,
+--	awful.layout.suit.spiral.dwindle,
 	awful.layout.suit.max.fullscreen,
-	awful.layout.suit.magnifier
+	awful.layout.suit.max,
+--	awful.layout.suit.magnifier
 }
 
 -- Define a tag table which hold all screen tags.
 tags = {}
 for s = 1, screen.count() do
-	-- Each screen has its own tag table.
-	tags[s] = awful.tag({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, s, layouts[2])
+	-- Order of layouts.
+	local order = { 1, 2, 3, 4, 5, 6, 7, 8, 9 }
+
+	-- Different defaults on screens.
+	local default = awful.layout.suit.tile;
+	if s == 2 then
+		default = awful.layout.suit.tile.bottom;
+	end
+
+	tags[s] = awful.tag(order, s, default)
 end
+
+-- Use maximized layout as default on first screen, first tag.
+awful.layout.set(awful.layout.suit.max, tags[1][1])
